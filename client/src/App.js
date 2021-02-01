f9ecec92efdddcbf904bb1801a19d13a0956b139
Shane Sutro <shanesutro@gmail.com>
2021-02-01 15:35:46 -0700

    Adding webpack and babel

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {hot} from "react-hot-loader";
+
+var App = (props) => {
+  return(
+  <div className="App">
+    <h1>Data Tracker: Alphabet Identification</h1>
+  </div>
+  )
+}
+
+export default hot(module)(App);
